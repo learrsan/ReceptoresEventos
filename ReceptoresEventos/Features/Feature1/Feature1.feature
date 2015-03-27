5240bd6ebc2e8f18f38557e705ff6a4aa1a96d16
learrsan <gaslas@hotmail.com>
2015-03-27 18:57:37 +0000

    27/06/15 18:57

---
+++ ReceptoresEventos/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="125c5056-7d93-4fbc-b306-48f95dc1a2f6" featureId="125c5056-7d93-4fbc-b306-48f95dc1a2f6" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ReceptoresEventos Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e8eb5615-b6bf-4509-bdf7-ca3e8ff092be" />
+  </projectItems>
+</feature>
---
+++ ReceptoresEventos/ReceptorEventosLista/ReceptorEventosLista.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace ReceptoresEventos.ReceptorEventosLista
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class ReceptorEventosLista : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item is being added.
+        /// </summary>
+        public override void ItemAdding(SPItemEventProperties properties)
+        {
+            base.ItemAdding(properties);
+        }
+
+        public override void itemUpdating(SPItemEventProperties properties)
+        {
+            base.ItemUpdating(properties);
+        }
+        
+        public override void itemDeleting(SPItemEventProperties properties)
+        {
+            base.ItemDeleting(properties);
+        }
+
+        private void ActualizacionPropiedades(SPWeb web, double precio)
+        {
+            string clave = "TotalImporte";
+
+            double actual = 0;
+
+            if (web.Properties[clave] != null)
+            {
+                actual = Convert.ToDouble(web.Properties[clave]);
+            }
+            else
+            {
+                web.Properties.Add(clave,"");
+            }
+            
+            actual += precio;
+
+            web.Properties[clave] = actual.ToString();
+            web.Properties.Update();
+        }
+    }
+}
